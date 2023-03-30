a823cadc3c9ec3b24472d42d58b469ec11aebd55
mingyuanhua <m.y.hua@hotmail.com>
2023-03-30 16:38:53 -0700

    Add a simple POST request

--- src/main/java/com/hmy/euclidean/HelloServlet.java
+++ src/main/java/com/hmy/euclidean/HelloServlet.java
@@ -1,8 +1,10 @@
 package com.hmy.euclidean;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
@@ -18,6 +20,20 @@ public class HelloServlet extends HttpServlet {
         response.getWriter().print(game);
     }
 
-    public void destroy() {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // read game info from request body
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String name = jsonRequest.getString("name");
+        float price = jsonRequest.getFloat("price");
+
+        // print game info to ide console
+        System.out.println("Name is: " + name);
+        System.out.println("Price is: " + price);
+
+        // return status = ok as response body to the client
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
     }
 }
