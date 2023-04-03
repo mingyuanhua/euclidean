172c6e4ca0826c59e2c8d75004e43f41e8bc2191
mingyuanhua <m.y.hua@hotmail.com>
2023-04-03 10:38:47 -0700

    Add EnableWebMvc Annotation

--- src/main/java/com/hmy/euclidean/ApplicationConfig.java
+++ src/main/java/com/hmy/euclidean/ApplicationConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
+@EnableWebMvc   // let spring to generate all objects that SpringMVC running required
 public class ApplicationConfig {
 }
