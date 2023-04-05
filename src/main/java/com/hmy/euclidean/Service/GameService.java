f29fcd7108149385b08516b2dec83e5d7b053340
mingyuanhua <m.y.hua@hotmail.com>
2023-04-05 11:29:52 -0700

    Add five strings to GameService

--- src/main/java/com/hmy/euclidean/Service/GameService.java
+++ src/main/java/com/hmy/euclidean/Service/GameService.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
+    private static final String TOKEN = "Bearer jonris96d3cj0a4nlkt398bgzch5au";
+    private static final String CLIENT_ID = "5l0ari2560dmkdrf5wql00ye5mk8nd";
+    private static final String TOP_GAME_URL = "https://api.twitch.tv/helix/games/top?first=%s";
+    private static final String GAME_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/games?name=%s";
+    private static final int DEFAULT_GAME_LIMIT = 20;
 }
