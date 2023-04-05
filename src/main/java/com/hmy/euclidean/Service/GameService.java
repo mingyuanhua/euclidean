1b45c7b6677b132ead6cf7ab9c4a36ab7f29ad6f
mingyuanhua <m.y.hua@hotmail.com>
2023-04-05 11:40:30 -0700

    Implement buildGameURL function

--- src/main/java/com/hmy/euclidean/Service/GameService.java
+++ src/main/java/com/hmy/euclidean/Service/GameService.java
@@ -2,6 +2,9 @@ package com.hmy.euclidean.Service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 @Service
 public class GameService {
     private static final String TOKEN = "Bearer jonris96d3cj0a4nlkt398bgzch5au";
@@ -9,4 +12,20 @@ public class GameService {
     private static final String TOP_GAME_URL = "https://api.twitch.tv/helix/games/top?first=%s";
     private static final String GAME_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/games?name=%s";
     private static final int DEFAULT_GAME_LIMIT = 20;
+
+    // build the request URL which will be used when calling Twitch APIs
+    // e.g. https://api.twitch.tv/helix/games/top when trying to get top games.
+    private String buildGameURL(String url, String gameName, int limit) {
+        if (gameName.equals("")) {
+            return String.format(url, limit);
+        } else {
+            try {
+                // Encode special characters in URL
+                gameName = URLEncoder.encode(gameName, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return String.format(url, gameName);
+        }
+    }
 }
