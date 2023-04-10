c985d19177bda50cf179873f8bda3ffef89a0ce3
mingyuanhua <m.y.hua@hotmail.com>
2023-04-10 08:14:06 -0700

    Connect GameController with GameService

--- src/main/java/com/hmy/euclidean/controller/GameController.java
+++ src/main/java/com/hmy/euclidean/controller/GameController.java
@@ -1,14 +1,18 @@
 package com.hmy.euclidean.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hmy.euclidean.Service.GameService;
+import com.hmy.euclidean.Service.TwitchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.rmi.server.ServerCloneException;
 
 // Use @Controller to mark a class its role as a web component,
 // so the spring mvc will register the methods which annotated the @RequestMapping.
@@ -20,7 +24,18 @@ public class GameController {
     private GameService gameService;
 
     @RequestMapping(value = "/game", method = RequestMethod.GET)
-    public void getGame(@RequestParam(value = "game_name", required = false) String gameName, HttpServletResponse response) throws IOException {
-
+    public void getGame(@RequestParam(value = "game_name", required = false) String gameName, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("application/json;charset=UTF-8");
+        try {
+            // Return the dedicated game information if gameName is provided in request URL,
+            // otherwise return the top x games
+            if (gameName != null) {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(gameService.searchGame(gameName)));
+            } else {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(gameService.topGames(0)));
+            }
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
     }
 }
