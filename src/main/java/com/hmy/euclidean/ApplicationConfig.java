e0a389e11a2a216a7b051a6dfbda24a952addbea
mingyuanhua <m.y.hua@hotmail.com>
2023-04-11 10:37:01 -0700

    Update euclidean servlet

--- src/main/java/com/hmy/euclidean/ApplicationConfig.java
+++ src/main/java/com/hmy/euclidean/ApplicationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc   // let spring to generate all objects that SpringMVC running required
+@EnableWebMvc
 public class ApplicationConfig {
 }
+
