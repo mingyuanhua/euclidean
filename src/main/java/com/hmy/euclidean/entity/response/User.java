6871e5b577de8a62eaa9ee167e4e6bcdd3f14cdf
mingyuanhua <m.y.hua@hotmail.com>
2023-04-16 15:56:43 -0700

    Add User Entity

---
+++ src/main/java/com/hmy/euclidean/entity/response/User.java
@@ -0,0 +1,67 @@
+package com.hmy.euclidean.entity.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "user_id")
+    @JsonProperty("user_id")
+    private String userId;
+
+    @JsonProperty("password")
+    private String password;
+
+    @Column(name = "first_name")
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @JsonProperty("last_name")
+    private String lastName;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
