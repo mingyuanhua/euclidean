75f5fd531abb4a83b1f55cd6e5254c18036dda99
mingyuanhua <m.y.hua@hotmail.com>
2023-04-18 16:48:00 -0700

    Create SearchController and Integrate with GameService

---
+++ src/main/java/com/hmy/euclidean/controller/SearchController.java
@@ -0,0 +1,22 @@
+package com.hmy.euclidean.controller;
+
+import com.hmy.euclidean.entity.db.Item;
+import com.hmy.euclidean.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SearchController {
+    @Autowired
+    private GameService gameService;
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, List<Item>> search(@RequestParam(value = "game_id") String gameId) {
+        return gameService.searchItems(gameId);
+    }
+}
