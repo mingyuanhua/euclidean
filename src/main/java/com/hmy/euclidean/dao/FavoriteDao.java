4513d7ce009c4a2f208030315ec68e585d76a082
mingyuanhua <m.y.hua@hotmail.com>
2023-04-18 15:32:00 -0700

    Create FavoriteDao under dao package

---
+++ src/main/java/com/hmy/euclidean/dao/FavoriteDao.java
@@ -0,0 +1,71 @@
+package com.hmy.euclidean.dao;
+
+import com.hmy.euclidean.entity.db.Item;
+import com.hmy.euclidean.entity.db.ItemType;
+import com.hmy.euclidean.entity.db.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class FavoriteDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    // Insert a favorite record to the database
+    public void setFavoriteItem(String userId, Item item) {
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+            User user = session.get(User.class, userId);
+            user.getItemSet().add(item);
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) session.close();
+        }
+    }
+
+    // Remove a favorite record from the database
+    public void unsetFavoriteItem(String userId, String itemId) {
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+            User user = session.get(User.class, userId);
+            Item item = session.get(Item.class, itemId);
+            user.getItemSet().remove(item);
+            session.beginTransaction();
+            session.update(user);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) session.close();
+        }
+    }
+
+    public Set<Item> getFavoriteItems(String userId) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            return session.get(User.class, userId).getItemSet();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (session != null) session.close();
+        }
+
+        return new HashSet<>();
+    }
+}
+
