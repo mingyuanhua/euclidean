24e289bcd81a91f9644d9f5df26a9e37a6a95d25
mingyuanhua <m.y.hua@hotmail.com>
2023-04-26 22:56:07 -0700

    Create LoginController

---
+++ src/main/java/com/hmy/euclidean/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.hmy.euclidean.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hmy.euclidean.entity.request.LoginRequestBody;
+import com.hmy.euclidean.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Controller
+public class LoginController {
+    @Autowired
+    private LoginService loginService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public void login(@RequestBody LoginRequestBody requestBody, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String firstName = loginService.verifyLogin(requestBody.getUserId(), requestBody.getPassword());
+
+        // Create a new session for the user if user ID and password are correct, otherwise return Unauthorized error
+        if (!firstName.isEmpty()) {
+            // Create a new session, put user ID as an attribute into the session object, and set the expiration time to 600 seconds
+            HttpSession session = request.getSession();
+            session.setAttribute("user_id", requestBody.getUserId());
+            session.setMaxInactiveInterval(600);
+
+            LoginRequestBody loginRequestBody = new LoginRequestBody(requestBody.getUserId(), firstName);
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().print(new ObjectMapper().writeValueAsString(loginRequestBody));
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+}
