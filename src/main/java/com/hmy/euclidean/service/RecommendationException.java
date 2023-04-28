c67bc9b16c22a4e44b92460aaaebb8371b0940e0
mingyuanhua <m.y.hua@hotmail.com>
2023-04-28 16:34:13 -0700

    Create RecommendationException

---
+++ src/main/java/com/hmy/euclidean/service/RecommendationException.java
@@ -0,0 +1,7 @@
+package com.hmy.euclidean.service;
+
+public class RecommendationException extends RuntimeException{
+    public RecommendationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
