df74fb5f5fb87898f392ef465b8ea8d86da84511
mingyuanhua <m.y.hua@hotmail.com>
2023-04-30 20:10:51 -0700

    Implement recommendByFavoriteHistory to recommend based-on favorite history

--- src/main/java/com/hmy/euclidean/service/RecommendationService.java
+++ src/main/java/com/hmy/euclidean/service/RecommendationService.java
@@ -7,8 +7,7 @@ import com.hmy.euclidean.entity.response.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class RecommendationService {
@@ -37,6 +36,40 @@ public class RecommendationService {
                     return recommendedItems;
                 }
                 recommendedItems.add(item);
+            }
+        }
+        return recommendedItems;
+    }
+
+    // Return a list of Item objects for the given type. Types are one of [Stream, Video, Clip]. All items are related to the items previously favorited by the user. E.g., if a user favorited some videos about game "Just Chatting", then it will return some other videos about the same game.
+    private List<Item> recommendByFavoriteHistory(Set<String> favoritedItemIds, List<String> favoritedGameIds, ItemType type) throws RecommendationException {
+        // Count the favorite game IDs from the database for the given user. E.g. if the favorited game ID list is ["1234", "2345", "2345", "3456"], the returned Map is {"1234": 1, "2345": 2, "3456": 1}
+        Map<String, Long> favoriteGameIdByCount = new HashMap<>();
+        for (String gameId : favoritedGameIds) {
+            favoriteGameIdByCount.put(gameId, favoriteGameIdByCount.getOrDefault(gameId, 0L) + 1);
+        }
+        // Sort the game Id by count. E.g. if the input is {"1234": 1, "2345": 2, "3456": 1}, the returned Map is {"2345": 2, "1234": 1, "3456": 1}
+        List<Map.Entry<String, Long>> sortedFavoriteGameIdListByCount = new ArrayList<>(favoriteGameIdByCount.entrySet());
+        sortedFavoriteGameIdListByCount.sort((Map.Entry<String, Long> e1, Map.Entry<String, Long> e2) -> Long.compare(e2.getValue(), e1.getValue()));
+        if (sortedFavoriteGameIdListByCount.size() > DEFAULT_GAME_LIMIT) {
+            sortedFavoriteGameIdListByCount = sortedFavoriteGameIdListByCount.subList(0, DEFAULT_GAME_LIMIT);
+        }
+        List<Item> recommendedItems = new ArrayList<>();
+        // Search Twitch based on the favorite game IDs returned in the last step
+        for (Map.Entry<String, Long> favoriteGame : sortedFavoriteGameIdListByCount) {
+            List<Item> items;
+            try {
+                items = gameService.searchByType(favoriteGame.getKey(), type, DEFAULT_PER_GAME_RECOMMENDATION_LIMIT);
+            } catch (TwitchException e) {
+                throw new RecommendationException("Failed to get recommendation result");
+            }
+            for (Item item : items) {
+                if (recommendedItems.size() == DEFAULT_TOTAL_RECOMMENDATION_LIMIT) {
+                    return recommendedItems;
+                }
+                if (!favoritedGameIds.contains(item.getId())) {
+                    recommendedItems.add(item);
+                }
             }
         }
         return recommendedItems;
